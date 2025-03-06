e38897f85864fcce5fa408fa94bc78da06028739
Kauan Santana <kauansbflu@gmail.com>
2025-03-06 14:56:19 -0300

    refactor Button component; move styles to separate CSS file and inject using unsafeCSS

--- src/components/button/Button.ts
+++ src/components/button/Button.ts
@@ -1,58 +1,10 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import buttonStyles from './button.css?inline'; // Importação do CSS como string
 
 @customElement('ds-button')
 export class Button extends LitElement {
-  static styles = css`
-    /* Estilos base do botão */
-    .button {
-      display: inline-block;
-      cursor: pointer;
-      border: 0;
-      border-radius: 5px;
-      font-weight: 700;
-      line-height: 1;
-      font-family: 'Inter', sans-serif;
-    }
-
-    /* Estilos para o botão primário */
-    .button--primary {
-      background-color: #0066ff;
-      width: 98px;
-      height: 37px;
-      color: white;
-    }
-
-    .button--primary:hover {
-      transition-duration: 300ms;
-      background-color: #ff8000;
-    }
-
-    .button--primary:active {
-      background-color: #c9c9c9;
-      color: #888888;
-    }
-
-    /* Estilos para o botão secundário */
-    .button--secondary {
-      background-color: #f3f3f3;
-      width: 98px;
-      height: 37px;
-      color: #333;
-    }
-
-    /* Estilos para tamanhos */
-    .button--small {
-      font-size: 12px;
-      padding: 8px 16px;
-    }
-
-    .button--medium {
-      font-size: 14px;
-      padding: 10px 20px;
-    }
-  `;
-
+  static styles = [unsafeCSS(buttonStyles)]; // Injeta o CSS no Shadow DOM
   /** Define se o botão é primário ou secundário */
   @property({ type: Boolean }) primary = false;
 
