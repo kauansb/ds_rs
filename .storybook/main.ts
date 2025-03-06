c2bc776f231441a0e472b1add44e3f75108f3d50
Kauan Santana <kauansbflu@gmail.com>
2025-03-06 15:46:13 -0300

    update Storybook configuration; simplify stories path and remove unused addons modify secondary button style; change background color to transparent

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,25 +2,16 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    //"../src/components/*.stories.@(js|jsx|ts|tsx)",
-    //"../src/**/*.mdx",
+    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
-    "@storybook/addon-designs"
+    "@chromatic-com/storybook"
   ],
   "framework": {
     "name": "@storybook/web-components-vite",
     "options": {}
-  },
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  docs: {
-    defaultName: 'Documentation',
-  },
+  }
 };
 export default config;
